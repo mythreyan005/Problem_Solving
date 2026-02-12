0912e54802fb6fd37ee85230b8fb2157f940b96a
mythreyan005 <mythreyansk@gmail.com>
2026-02-12 23:55:17 +0530

    feb122026

---
+++ BinarySearch.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int n=nums.length;
+        int l=0;
+        int h=n-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(nums[mid]==target)
+            return mid;
+            else if(nums[mid]<target)
+            l=mid+1;
+            else if(nums[mid]>target)
+            h=mid-1;
+
+        }
+        return -1;
+    }
+}
