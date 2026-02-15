87061429755eac9454388bfa384578a4d655a9f6
mythreyan005 <mythreyansk@gmail.com>
2026-02-15 20:43:01 +0530

    feb152026

---
+++ prb1929.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] nums2 = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            nums2[i] = nums[i];        
+            nums2[i + n] = nums[i];    
+        }
+
+        return nums2;
+    }
+}
